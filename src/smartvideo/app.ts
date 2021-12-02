843c01bfcdc97bb8ceef92f0581e94aeb6236944
javoliful <javier.oliverfulguera@aareon.com>
2021-12-02 17:09:15 +0100

    * cors

--- src/smartvideo/app.ts
+++ src/smartvideo/app.ts
@@ -1,8 +1,7 @@
 import * as express from 'express';
 import {createServer} from 'http';
 import {Server} from 'socket.io';
-import { SocketIoSubscriber } from './eventListeners/subscriber';
-
+import {SocketIoSubscriber} from './eventListeners/subscriber';
 
 export class SmartVideo {
 
@@ -13,27 +12,16 @@ export class SmartVideo {
     constructor() {
         this.app = express();
         this.app.use(express.static('public'));
-        this.app.use(function(req, res, next) {
-            res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-            // Add this
-            if (req.method === 'OPTIONS') {
-
-                res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, OPTIONS');
-                res.header('Access-Control-Max-Age', '120');
-                return res.status(200).json({});
-            }
-
-            next();
-
-        });
         this.port = process.env.PORT || SmartVideo.PORT;
         const httpServer = createServer(this.app);
 
       httpServer.listen(this.port, () => {
             console.log('Running smartvideo-io on port %s', this.port);
-            let io = new Server(httpServer);
+            let io = new Server(httpServer, {cors: {
+                origin: "https://smartvideo-documentation.herokuapp.com",
+                //origin: "http://localhost:8000",
+                methods: ["GET", "POST"]
+            }});
             const subscriber = new SocketIoSubscriber();
             subscriber.subscribe(io);
         });
